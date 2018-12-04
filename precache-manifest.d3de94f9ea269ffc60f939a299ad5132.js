f4f88dcb7febcd8d0b81d0e6ed62441cc62ed909
Andrew Chin <chin.andrew@outlook.com>
2018-12-03 22:54:53 -0500

    Updates

---
+++ precache-manifest.d3de94f9ea269ffc60f939a299ad5132.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "14eb94eb546ac172d1d7c5da8a09ef22",
+    "url": "/static/media/hiragana-ke.14eb94eb.svg"
+  },
+  {
+    "revision": "fbaf91d67f86815db48b",
+    "url": "/static/css/main.95c4e9f2.chunk.css"
+  },
+  {
+    "revision": "8fae05dd6afb29b249f1",
+    "url": "/static/js/1.8fae05dd.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e10e4084db875468105dd18140fc1621",
+    "url": "/static/media/hiragana-a.e10e4084.svg"
+  },
+  {
+    "revision": "cb188bc625c3a13b15b6d2c8769d91c3",
+    "url": "/static/media/hiragana-e.cb188bc6.svg"
+  },
+  {
+    "revision": "9273d89aca23a87a9482e5a3294230cd",
+    "url": "/static/media/hiragana-i.9273d89a.svg"
+  },
+  {
+    "revision": "cdf9c2e5507211047422a004efda1869",
+    "url": "/static/media/hiragana-ka.cdf9c2e5.svg"
+  },
+  {
+    "revision": "fbaf91d67f86815db48b",
+    "url": "/static/js/main.fbaf91d6.chunk.js"
+  },
+  {
+    "revision": "d4c6129b2004bf2e5ef1fe1ac7a70b78",
+    "url": "/static/media/hiragana-ki.d4c6129b.svg"
+  },
+  {
+    "revision": "3dac2be1f8a8df20e24d3de331a0510c",
+    "url": "/static/media/hiragana-ko.3dac2be1.svg"
+  },
+  {
+    "revision": "d1ebab0f56847770ca93923a52cb5f00",
+    "url": "/static/media/hiragana-ku.d1ebab0f.svg"
+  },
+  {
+    "revision": "f1a6a6d5f2e57f1fb6dbec6763a7ca12",
+    "url": "/static/media/hiragana-o.f1a6a6d5.svg"
+  },
+  {
+    "revision": "dd23115310dc02ddcabfc54e3cbc3218",
+    "url": "/static/media/hiragana-u.dd231153.svg"
+  },
+  {
+    "revision": "4c3274ca5a0626cc80475ab42588528d",
+    "url": "/static/media/katakana-a.4c3274ca.svg"
+  },
+  {
+    "revision": "78ee9853d281021acc5cdc21df14bd10",
+    "url": "/index.html"
+  }
+];
