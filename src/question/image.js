1ca27a109d1dd0f8fc130cb2e8c58ebe02b087df
Andrew Chin <chin.andrew@outlook.com>
2018-12-09 23:13:22 -0500

    Fix images in chrome

--- src/question/image.js
+++ src/question/image.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './image.css'
 
 export default (imagePath, className) => {
-  const reqSvgs = require.context ( '../images', true, /\.svg$/ )
+  const reqSvgs = require.context ( '../images', true, /(\.svg)|(\.png)$/ )
   return <img className={className} src={reqSvgs(imagePath)} />
 }
 
